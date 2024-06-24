b74f5f15a4b6515f8cea20569c2d3aa89658cebf
Elwood Nelson <elwoodnelson@gmail.com>
2024-06-24 12:39:43 -0500

    First two exercise plus start of 3

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,15 +15,21 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
-  return;
-}
+  return `Hello, ${name}!`;
+}  // end helloName
+
 // Remember to call the function to test
+console.log(helloName('Elwood'));
 
 
 // 3. Function to add two numbers together & return the result
-function addNumbers(firstNumber) {
+function addNumbers(firstNumber,secondNumber) {
+  let result = firstNumber + secondNumber;
+  return `The result of ${firstNumber} + ${secondNumber} is ${result}`;
   // return firstNumber + secondNumber;
-}
+} // end addNumbers
+
+console.log(addNumbers(3,4));
 
 
 // 4. Function to multiply three numbers & return the result
